35df27a313910b08701f25f3cc38525960b2e56f
Amir Maleki <amirmaleki@Amirs-MacBook-Pro.local>
2024-06-10 19:48:23 +0200

    bug fix2

--- NLQ/VSLBase/main.py
+++ NLQ/VSLBase/main.py
@@ -154,7 +154,7 @@ def main(configs, parser):
                 # generate mask
                 video_mask = convert_length_to_mask(vfeat_lens).to(device)
                 # compute logits
-                h_score, start_logits, end_logits = model(
+                start_logits, end_logits = model(
                     word_ids, char_ids, vfeats, video_mask, query_mask
                 )
                 # compute loss
